0cba86e4d746fc60e171c67703580dad26588fc4
Yf-Faina <1654289166@qq.com>
2024-06-07 19:42:49 +0800

    add 6 pasages

--- assets/pageview-Bpr31ZxL.js
+++
@@ -1,7 +0,0 @@
-import{_ as r}from"./app-BofQRgQ0.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://yf-faina-github-io.vercel.app/"};const o=async()=>{try{const{pageviewCount:e}=await r(()=>import("./app-BofQRgQ0.js").then(t=>t.P),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!")}};export{o as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
